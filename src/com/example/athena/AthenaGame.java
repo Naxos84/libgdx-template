1519e6f5c526cb8870dbd412618930949115f624
Matthias Kolley <matthias.kolley@googlemail.com>
2018-11-21 07:52:21 +0100

    Lösung Stunde 5
    inkl. Reset Bonus (Space setzt alle Werte zurück)

--- src/com/example/athena/AthenaGame.java
+++ src/com/example/athena/AthenaGame.java
@@ -2,10 +2,13 @@ package com.example.athena;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.input.*;
 
 public class AthenaGame extends Game {
 
@@ -13,8 +16,13 @@ public class AthenaGame extends Game {
     private OrthographicCamera camera;
     //Batch zum rendern von Texturen etc.
     private SpriteBatch batch;
-    //Textur (Bild) die gerendert werden soll
-    private Texture tex;
+    //TextureRegion (Bild) die gerendert werden soll
+    private TextureRegion textureRegion;
+    int posx;
+    int posy;
+    int width;
+    int height;
+    int rotation;
 
     @Override
     public void create() {
@@ -26,8 +34,19 @@ public class AthenaGame extends Game {
         camera.setToOrtho(false, 800, 600);
         //Erstellen des Batches
         batch = new SpriteBatch();
-        //Erstellen der Textur(Bild) mit "characters.png"
-        tex = new Texture(Gdx.files.internal("sprites/characters.png"));
+        //Erstellen der TextureRegion(Bild) mit "characters.png"
+        textureRegion = new TextureRegion(new Texture(Gdx.files.internal("sprites/characters.png")));
+        //Alle Werte einmal festlegen, damit das Bild gemalt werden kann
+        initializeValues();
+    }
+
+    //Kann aufgerufen werden um fest definierte Werte zu nutzen
+    private void initializeValues() {
+        posx = 0;
+        posy = 0;
+        width = textureRegion.getRegionWidth();
+        height = textureRegion.getRegionHeight();
+        rotation = 0;
     }
 
     @Override
@@ -36,10 +55,49 @@ public class AthenaGame extends Game {
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            //X-Positions Wert um 1 verringern
+            posx--;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            //X-Positions Wert um 1 erhöhen
+            posx++;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            //Y-Positions Wert um 1 verringern
+            posy--;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            //Y-Positions Wert um 1 erhöhen
+            posy++;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.PLUS)) {
+            //Breite und Höhe um 1 erhöhen
+            width++;
+            height++;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.MINUS)) {
+            //Breite und Höhe um 1 verringern
+            width--;
+            height--;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.PAGE_UP)) {
+            //Drehung um 1 erhöhen (nach rechts drehen um 1°)
+            rotation++;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.PAGE_DOWN)) {
+            //Dehung um 1 verringern (nach links drehen um 1°)
+            rotation--;
+        }
+        //#####
+        //BONUS
+        //#####
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            initializeValues();
+        }
+
+
+
         //Bevor ein Bild gerendert werden kann, muss der Batch gestartet werden
         batch.begin();
-        //Rendern des Bildes an der Stelle (0,0) (links unten)
-        batch.draw(tex, 0, 0);
+        //Rendern des Bildes an der Stelle (posx,posy) mit der Höhe (height) der Breite (width) und der Drehung (rotation)
+        batch.draw(textureRegion, posx, posy,0, 0, width, height, 1, 1, rotation);
         //Nachdem alles gerendet wurde, muss der Batch beendet werden.
         batch.end();
     }
@@ -50,7 +108,7 @@ public class AthenaGame extends Game {
         //Hier müssen einige Sachen wieder freigegeben werden, damit die nach Beenden des Spiels nicht mehr im Speicher bleiben!
 
         //Speicher von der Textur wieder freigeben!
-        tex.dispose();
+        textureRegion.getTexture().dispose();
         //Speicher vom Batch wieder freigeben!
         batch.dispose();
     }
