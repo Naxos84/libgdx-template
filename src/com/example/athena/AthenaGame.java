df9e8e54076b34b3ffd4f9eff682ed0a986ab1be
Florian Bottke <florian.bottke@clausohm.de>
2019-01-30 08:47:06 +0100

    Stunde 9.

--- src/com/example/athena/AthenaGame.java
+++ src/com/example/athena/AthenaGame.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
@@ -17,6 +18,10 @@ public class AthenaGame extends Game {
 
     private static final int GRID_WIDTH = 16;
     private static final int GRID_HEIGHT = 16;
+
+    private static final float WALK_SPEED = 8;
+
+    private static final String COLLISION_LAYER = "Collision";
 
     // Camera for scene
     private OrthographicCamera camera;
@@ -26,6 +31,10 @@ public class AthenaGame extends Game {
 
     // Texture region for rendering
     private TextureRegion textureRegion;
+    private TextureRegion textureRegionUp;
+    private TextureRegion textureRegionDown;
+    private TextureRegion textureRegionLeft;
+    private TextureRegion textureRegionRight;
 
     // Tiled map for store the map data in memory
     TiledMap tiledMap;
@@ -40,9 +49,17 @@ public class AthenaGame extends Game {
     int height;
 
     //grid-cells per second
-    private static final float WALKSPEED = 2;
     float walkTimer = 0;
     boolean canWalk = false;
+
+    Direction currentDirection;
+
+    enum Direction {
+        Up,
+        Down,
+        Left,
+        Right
+    }
 
     @Override
     public void create() {
@@ -58,9 +75,17 @@ public class AthenaGame extends Game {
 
         // Create texture region from image characters.png
         textureRegion = new TextureRegion(new Texture(Gdx.files.internal("sprites/characters.png")), 0, 0, GRID_WIDTH, GRID_HEIGHT);
+        textureRegionUp = new TextureRegion(new Texture(Gdx.files.internal("sprites/characters.png")), 16, 48, GRID_WIDTH, GRID_HEIGHT);
+        textureRegionDown = new TextureRegion(new Texture(Gdx.files.internal("sprites/characters.png")), 16, 0, GRID_WIDTH, GRID_HEIGHT);
+        textureRegionLeft = new TextureRegion(new Texture(Gdx.files.internal("sprites/characters.png")), 16, 16, GRID_WIDTH, GRID_HEIGHT);
+        textureRegionRight = new TextureRegion(new Texture(Gdx.files.internal("sprites/characters.png")), 16, 32, GRID_WIDTH, GRID_HEIGHT);
 
         // Load start map and init render
         tiledMap = new TmxMapLoader().load("maps/start.tmx");
+
+        TiledMapTileLayer collisionLayer = (TiledMapTileLayer) tiledMap.getLayers().get(COLLISION_LAYER);
+        collisionLayer.setVisible(false);
+
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
 
         // Set initial values
@@ -71,10 +96,26 @@ public class AthenaGame extends Game {
     private void initializeValues() {
 
         x = 0;
-        y = 0;
+        y = 12 * GRID_HEIGHT;
 
         width = textureRegion.getRegionWidth();
         height = textureRegion.getRegionHeight();
+
+        currentDirection = Direction.Down;
+    }
+
+    public boolean isBlocked(int x, int y) {
+
+        // Convert player pixel coordinates to grid coordinates
+        x = x / GRID_WIDTH;
+        y = y / GRID_HEIGHT;
+
+        // Get collision layer and cell
+        TiledMapTileLayer collisionLayer = (TiledMapTileLayer) tiledMap.getLayers().get(COLLISION_LAYER);
+        TiledMapTileLayer.Cell cell = collisionLayer.getCell(x, y);
+
+        // Return true if cell is blocked
+        return cell != null;
     }
 
     // Called on each time a new frame is rendered
@@ -83,41 +124,58 @@ public class AthenaGame extends Game {
 
         walkTimer -= Gdx.graphics.getDeltaTime();
         if (walkTimer < 0) {
-            walkTimer = 1 / WALKSPEED;
+            walkTimer = 1 / WALK_SPEED;
             canWalk = true;
         }
-
 
         // Clear background with background color
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // Walking
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            currentDirection = Direction.Left;
+
             if (canWalk) {
-                x -= 16;
-                canWalk = false;
+                if (!isBlocked(x - GRID_WIDTH, y)) {
+                    x -= GRID_WIDTH;
+                    canWalk = false;
+                }
             }
         } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            if (canWalk) {
-                x += 16;
-                canWalk = false;
-            }
-        }
+            currentDirection = Direction.Right;
 
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
             if (canWalk) {
-                y -= 16;
-                canWalk = false;
+
+                if (!isBlocked(x + GRID_WIDTH, y)) {
+                    x += GRID_WIDTH;
+                    canWalk = false;
+                }
+            }
+        } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            currentDirection = Direction.Down;
+
+            if (canWalk) {
+
+                if (!isBlocked(x, y - GRID_HEIGHT)) {
+                    y -= GRID_HEIGHT;
+                    canWalk = false;
+                }
             }
         } else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            currentDirection = Direction.Up;
+
             if (canWalk) {
-                y += 16;
-                canWalk = false;
+
+                if (!isBlocked(x, y + GRID_HEIGHT)) {
+                    y += GRID_HEIGHT;
+                    canWalk = false;
+                }
             }
         }
 
         // Set camera position to player an update camera
-        camera.position.set(x, y,  0);
+        camera.position.set(x, y, 0);
         camera.update();
 
         // Set projection matrix from camera to batch
@@ -129,13 +187,36 @@ public class AthenaGame extends Game {
 
         // Render sprite batch with character
         batch.begin();
-        batch.draw(textureRegion, x, y, width, height);
+
+        switch (currentDirection) {
+            case Up: {
+                batch.draw(textureRegionUp, x, y, width, height);
+                break;
+            }
+            case Down: {
+                batch.draw(textureRegionDown, x, y, width, height);
+                break;
+            }
+            case Left: {
+                batch.draw(textureRegionLeft, x, y, width, height);
+                break;
+            }
+            case Right: {
+                batch.draw(textureRegionRight, x, y, width, height);
+                break;
+            }
+        }
+
         batch.end();
     }
 
     @Override
     public void dispose() {
         textureRegion.getTexture().dispose();
+        textureRegionUp.getTexture().dispose();
+        textureRegionDown.getTexture().dispose();
+        textureRegionLeft.getTexture().dispose();
+        textureRegionRight.getTexture().dispose();
         batch.dispose();
     }
 }
