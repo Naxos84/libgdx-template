e57a722542ec8a18cdd0f8e1f8b3ca112ab67bf0
Florian Bottke <florian.bottke@clausohm.de>
2019-02-26 09:19:14 +0100

    Stunde 10 - Implement warp.

--- src/com/example/athena/AthenaGame.java
+++ src/com/example/athena/AthenaGame.java
@@ -8,11 +8,16 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
 
 public class AthenaGame extends Game {
 
@@ -72,7 +77,7 @@ public class AthenaGame extends Game {
 
         // Create view port for camera
         camera.setToOrtho(false, 400, 300);
-        //Erstellen des Batches
+
         // Create sprite batch
         batch = new SpriteBatch();
 
@@ -84,15 +89,7 @@ public class AthenaGame extends Game {
         textureRegionRight = new TextureRegion(new Texture(Gdx.files.internal("sprites/characters.png")), 16, 32, GRID_WIDTH, GRID_HEIGHT);
 
         // Load start map and init render
-        tiledMap = new TmxMapLoader().load("maps/start.tmx");
-
-        indexOfOverLayer = tiledMap.getLayers().getIndex(OVER_LAYER);
-
-        TiledMapTileLayer collisionLayer = (TiledMapTileLayer) tiledMap.getLayers().get(COLLISION_LAYER);
-        collisionLayer.setVisible(false);
-
-        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-
+        loadMap("maps/start.tmx");
 
         // Set initial values
         initializeValues();
@@ -122,6 +119,94 @@ public class AthenaGame extends Game {
 
         // Return true if cell is blocked
         return cell != null;
+    }
+
+    private class Warp {
+
+        private Rectangle warpZone;
+
+        private String map;
+
+        private Integer destX;
+        private Integer destY;
+
+        Warp(float x, float y, float width, float height, String map, Integer destX, Integer destY) {
+
+            this.warpZone = new Rectangle(x, y, width, height);
+
+            this.map = map;
+            this.destX = destX;
+            this.destY = destY;
+        }
+    }
+
+    private void loadMap(String filename) {
+
+        if (tiledMap != null) {
+            tiledMap.dispose();
+        }
+
+        tiledMap = new TmxMapLoader().load(filename);
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
+        // Get indices
+        indexOfOverLayer = tiledMap.getLayers().getIndex(OVER_LAYER);
+
+        // Set visibility off
+        tiledMap.getLayers().get(indexOfOverLayer).setVisible(false);
+
+        TiledMapTileLayer collisionLayer = (TiledMapTileLayer) tiledMap.getLayers().get(COLLISION_LAYER);
+        collisionLayer.setVisible(false);
+    }
+
+    private void checkWarps() {
+        Array<Warp> warps = new Array<>();
+        MapLayer objectLayer = tiledMap.getLayers().get("Objects");
+
+        for (MapObject mapObject : objectLayer.getObjects()) {
+
+            if (mapObject.getProperties().containsKey("type")) {
+
+                String type = mapObject.getProperties().get("type", String.class);
+
+                if (type.equals("WARP")) {
+                    String map = mapObject.getProperties().get("DEST_MAP", String.class);
+                    Float posX = mapObject.getProperties().get("x", Float.class);
+                    Float posY = mapObject.getProperties().get("y", Float.class);
+                    Float width = mapObject.getProperties().get("width", Float.class);
+                    Float height = mapObject.getProperties().get("height", Float.class);
+                    Integer dest_x = mapObject.getProperties().get("DEST_X", Integer.class);
+                    Integer dest_y = mapObject.getProperties().get("DEST_Y", Integer.class);
+
+                    warps.add(new Warp(posX / GRID_WIDTH,
+                            posY / GRID_HEIGHT,
+                            width / GRID_WIDTH,
+                            height / GRID_HEIGHT,
+                            map,
+                            dest_x,
+                            dest_y));
+                }
+            }
+        }
+
+        Rectangle playerRect = new Rectangle(x / GRID_WIDTH, y / GRID_HEIGHT, 1, 1);
+//        Gdx.app.debug("playerRect", player.getX() + ":" + player.getY() + " - " + player.getWidth() + ":" + player.getHeight());
+        for (Warp warp : warps) {
+
+            if (Intersector.overlaps(playerRect, warp.warpZone)) {
+
+                loadMap("maps/" + warp.map + ".tmx");
+
+                Integer mapWidth = tiledMap.getProperties().get("width", Integer.class);
+                Integer mapHeight = tiledMap.getProperties().get("height", Integer.class);
+
+                int dest_x = warp.destX;
+                int dest_y = mapHeight - warp.destY - 1;
+
+                x = dest_x * GRID_WIDTH;
+                y = dest_y * GRID_HEIGHT;
+            }
+        }
     }
 
     // Called on each time a new frame is rendered
@@ -179,6 +264,9 @@ public class AthenaGame extends Game {
                 }
             }
         }
+
+        // Check warps
+        checkWarps();
 
         // Set camera position to player an update camera
         camera.position.set(x, y, 0);
